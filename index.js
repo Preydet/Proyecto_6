a6c30595b189e06813ef803bfc394ee3c037dc5c
Preydet <pareydet@gmail.com>
2025-12-27 17:26:36 -0300

    Conectado a MongoDB

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./src/config/db");
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+connectDB();
+
+//middlewares
+app.use(express.json());
+
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto` + PORT) ;
+});
